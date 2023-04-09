bda0f718f5d5dc9fa75968af6d410d88a1ebd4b8
kunwarvk01 <kunwarvk01@gmail.com>
2023-04-09 12:17:51 +0530

    added update liked functionality

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -24,6 +24,7 @@ export const createPost = async (req, res) => {
 
     } catch (err) {
         res.status(500).json({ message: err.message });
+    }
 }
 
 /* READ */
@@ -36,12 +37,38 @@ export const getFeedPosts = async (req, res) => {
     }
 }
 
-export const getUserPosts = async (req, res) => {
-    
+export const getUserPosts = async (req, res) => { 
     try {
         const { userId } = req.params;
         const post = await Post.find({ userId });
         res.status(200).json(post);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+}
+
+/* UPDATE */
+export const likePost = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const { userId } = req.body; 
+        const post = await Post.findById(id); //grabbing the post
+        const isLiked = await post.likes.get(userId); //checking if user has liked 
+
+        if(isLiked) {
+            post.likes.delete(userId);
+        } else {
+            post.likes.set(userId, true);
+        }
+
+        const updatedPost = await Post.findByIdAndUpdate(
+            id,
+            { liked: post.likes },
+            { new: true }
+        ); // updating the post with new likes
+        
+        res.status(200).json(updatedPost);
+
     } catch (err) {
         res.status(404).json({ message: err.message });
     }
