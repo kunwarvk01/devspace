435b8397d6f06dced10d1837393da518cf1cd0c3
kunwarvk01 <kunwarvk01@gmail.com>
2023-04-15 14:36:14 +0530

    added appropriate comments to routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,10 @@
-import express from "express";
-import { login } from "../controllers/auth.js";
+import express from "express";// Import the express library to use for creating the router
+import { login } from "../controllers/auth.js"; // Import the login function from the auth controller
 
+// Create a new router instance using the express.Router() method
 const router = express.Router();
-
+// Define a route for handling login requests with a POST method, and call the login function from the auth controller
 router.post("/login", login);
+
 
 export default router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -9,10 +9,13 @@ import { verifyToken } from "../middleware/auth.js";
 const router = express.Router();
 
 /* READ */
+// GET user information by id
 router.get("/:id", verifyToken, getUser);
+// GET user's friend list by id
 router.get("/:id/friends", verifyToken, getUserFriends);
 
 /* UPDATE */
+// PATCH (add/remove) friend for a user by id and friendId
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
