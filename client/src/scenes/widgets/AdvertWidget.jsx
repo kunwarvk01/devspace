d121141f6f5dedd2e06f4cd7fddf43dd16717ea8
kunwarvk01 <kunwarvk01@gmail.com>
2023-04-20 15:44:51 +0530

    advertisement configured

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -20,12 +20,12 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:3001/assets/info4.jpeg"
+        src="http://localhost:3001/assets/ad.png"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
-        <Typography color={main}>MikaCosmetics</Typography>
-        <Typography color={medium}>mikacosmetics.com</Typography>
+        <Typography color={main}>TechStack</Typography>
+        <Typography color={medium}>techstack.com</Typography>
       </FlexBetween>
       <Typography
         color={medium}
@@ -37,8 +37,7 @@ const AdvertWidget = () => {
           },
         }}
       >
-        Your pathway to stunning and immaculate beauty and made sure your skin
-        is exfoliating skin and shining like light.
+        A course on AWS Authentication focuses on securing cloud infrastructure by teaching the fundamentals of authentication mechanisms available on AWS. 
       </Typography>
     </WidgetWrapper>
   );
